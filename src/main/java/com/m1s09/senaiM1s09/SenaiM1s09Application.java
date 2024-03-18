96980cf6e91e3c9e3560a5f2cc40ebe5de4ab32f
Marcos Vinicios <mvdias1268@gmail.com>
2024-03-17 21:33:02 -0300

    first commit

---
+++ src/main/java/com/m1s09/senaiM1s09/SenaiM1s09Application.java
@@ -0,0 +1,13 @@
+package com.m1s09.senaiM1s09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiM1s09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaiM1s09Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/m1s09/senaiM1s09/SenaiM1s09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1s09.senaiM1s09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaiM1s09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
